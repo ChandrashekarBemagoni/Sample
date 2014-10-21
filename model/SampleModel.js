0ac6b9eb6773fda82f4dac00eed6b5bbf2a7b1da
chandrashekar <chandrashekar.bemagoni@gmail.com>
2014-10-21 14:17:24 +0530

    minor formatted

---
+++ model/SampleModel.js
@@ -0,0 +1,30 @@
+/**
+ * Created by chandrashekar on 10/21/2014.
+ */
+
+
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var PropertySet = require( 'AXON/PropertySet' );
+  var inherit = require( 'PHET_CORE/inherit' );
+
+
+  function SampleModel() {
+
+    PropertySet.call( this, {
+        isPlay: true,
+        speed: 'normal' //speed of the model, either 'normal' or 'slow'
+      }
+    );
+
+  }
+
+  return inherit( PropertySet, SampleModel, {
+    // reset
+    reset: function() {
+      PropertySet.prototype.reset.call( this );
+    }
+  } );
+} );
